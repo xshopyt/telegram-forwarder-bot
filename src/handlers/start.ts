9f7aceed5401dadbfb36575fef2f23aa4e87d786
Gaurav Sharma <117282153+xshopyt@users.noreply.github.com>
2024-03-05 21:49:29 +0530

    Update start.ts

--- src/handlers/start.ts
+++ src/handlers/start.ts
@@ -2,7 +2,7 @@ import { BotContext } from "../bot";
 
 export default async function start_handler(ctx: BotContext) {
     await ctx.reply(
-        `Hello, I am a bot that forwards messages from one chat to another. I only work for the owner of this bot. If you want to create a bot like me, just create a bot on @BotFather and foward the message with bot token to me. I will clone the new bot and set you as the owner of that bot.\n\nIf you are the owner of this bot, you can forward messages from one chat to another learn more using /help command.`,
+        `Hello, I am a bot that forwards messages from one chat to another. I only work for the owner of this bot.\n\nIf you are the owner of this bot, you can forward messages from one chat to another learn more using /help command.`,
         {
             reply_markup: {
                 inline_keyboard: [
